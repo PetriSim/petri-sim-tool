1ef70634296e099bc808d7e96af1652b9d7fc719
FuratH <hamdanfurat@gmail.com>
2022-11-19 20:46:08 +0100

    add electron to project

---
+++ public/electron.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+function createWindow() {
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '../index.html'),
+        protocol: 'file:',
+        slashes: true,
+    });
+    /*
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });*/
+
+    win = new BrowserWindow({
+        show: false,
+        minWidth: 1100,
+        minHeight: 800,
+    });
+    win.maximize();
+    win.show();
+
+    win.loadURL(startUrl);
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    });
+}
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+});
