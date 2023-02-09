26a165a63731a2e1728b48efff6cc0ac47bcf438
FuratH <hamdanfurat@gmail.com>
2023-02-09 18:03:03 +0100

    rewrite event as a functional component :memo:

--- frontend/src/components/EditorSidebar/Modelbased/Event.jsx
+++ frontend/src/components/EditorSidebar/Modelbased/Event.jsx
@@ -1,174 +1,149 @@
-import { Input, FormControl, FormLabel, Flex, Button, Stack, Select, Box, ButtonGroup, IconButton, Text} from '@chakra-ui/react';
-import { AddIcon, MinusIcon } from '@chakra-ui/icons'
-import React from 'react';
+import { Input, FormControl, FormLabel, Flex, Button, Stack, Select, Box, ButtonGroup, IconButton, Text } from '@chakra-ui/react';
+import { AddIcon, MinusIcon } from '@chakra-ui/icons';
+import React, { useState, useEffect } from 'react';
 
-class Event extends React.Component {
-  constructor(props) {
-    super(props);
+const Event = (props) => {
+  const [interArrivalTime, setInterArrivalTime] = useState("");
+  const [values, setValues] = useState("");
+  const [unit, setUnit] = useState("");
+  const [distributionType, setDistributionType] = useState("");
+  const [distributionTypes, setDistributionTypes] = useState([
+    { distribution_name: "exponential", distribution_params: ["mean"] },
+    { distribution_name: "normal", distribution_params: ["mean", "standard deviation"] },
+    { distribution_name: "uniform", distribution_params: ["min", "max"] },
+    { distribution_name: "constant", distribution_params: ["constant value"] },
+    { distribution_name: "erlang", distribution_params: ["order", "mean"] },
+    { distribution_name: "triangular", distribution_params: ["lower", "peak", "upper"] },
+    { distribution_name: "binomial", distribution_params: ["probabiliy", "amount"] },
+    { distribution_name: "arbitraryFiniteProbabilityDistribution", distribution_params: [] },
+  ]);
+  const [distributionValues, setDistributionValues] = useState([]);
+  const [currentEvent, setCurrentEvent] = useState({});
 
-    this.state = {
-      interArrivalTime:"",
-      values:"",
-      unit:"",
-      distributionType: "",
-      distributionTypes: [
-                              {distribution_name: "exponential", distribution_params: ["mean"]},
-                              {distribution_name: "normal", distribution_params: ["mean", "standard deviation"]},
-                              {distribution_name: "uniform", distribution_params: ["min", "max"]},
-                              {distribution_name: "constant", distribution_params: ["constant value"]},
-                              {distribution_name: "erlang", distribution_params: ["order", "mean"]},
-                              {distribution_name: "triangular", distribution_params: ["lower", "peak", "upper"]},
-                              {distribution_name: "binomial", distribution_params: ["probabiliy", "amount"]},
-                              {distribution_name: "arbitraryFiniteProbabilityDistribution", distribution_params: []}
-                           ],
-      distributionValues: [],
-      currentEvent: {}
-    };
+  useEffect(() => {
+    const currentEvent = props.getData("currentModel").modelParameter.events.find(
+      (value) => value.id === props.selectedObject.id
+    );
 
-    this.onSubmit = this.onSubmit.bind(this);
-    this.changeValueAmount = this.changeValueAmount.bind(this);
-    this.handleKeyChange = this.handleKeyChange.bind(this); 
-    this.handleInputChange = this.handleInputChange.bind(this);    
-  }
+    if (currentEvent === undefined) {
+      props.setDataObj({});
+      return;
+    }
 
-    componentDidMount(){
-      const currentEvent = this.props.getData("currentModel").modelParameter.events.find(
-        value => value.id === this.props.selectedObject.id
-      );
+    setCurrentEvent(currentEvent);
 
-      if(currentEvent === undefined){
-        this.props.setDataObj({})
-        return
-      }
+    let newTypes = distributionTypes;
 
+    newTypes.find((dis) => dis.distribution_name === currentEvent.interArrivalTime.distributionType).distribution_params = currentEvent.interArrivalTime.values.map((v) => v.id);
 
-      this.setState({currentEvent: currentEvent})
+    setInterArrivalTime(currentEvent.interArrivalTime);
+    setValues(currentEvent.interArrivalTime.values);
+    setUnit(currentEvent.unit);
+    setDistributionType(currentEvent.interArrivalTime.distributionType);
+    setDistributionValues(currentEvent.interArrivalTime.values.map((v) => v.value));
+    setDistributionTypes(newTypes);
+  }, []);
 
-
-      let newTypes = this.state.distributionTypes
-
-      
-       newTypes.find(dis => dis.distribution_name === currentEvent.interArrivalTime.distributionType).distribution_params = currentEvent.interArrivalTime.values.map(v => v.id) 
-        
-      
-      this.setState({
-          interArrivalTime: currentEvent.interArrivalTime,
-          values: currentEvent.interArrivalTime.values,
-          unit: currentEvent.unit,
-          distributionType: currentEvent.interArrivalTime.distributionType,
-          distributionValues: currentEvent.interArrivalTime.values.map(v => v.value),
-          distributionTypes: newTypes
-        })
-
-  }
-
-  handleInputChange(resource, index) {
+  const handleInputChange = (resource, index) => {
     const target = resource.target;
     const value = target.value;
     const name = target.name;
 
-    if(name === "distributionType"){
-      this.setState({
-          distributionValues: new Array(this.state.distributionTypes.find(dis => dis.distribution_name === value).distribution_params.length).fill(0)
-      })  
-  }
+    if (name === "distributionType") {
+      setDistributionValues(
+        new Array(
+          distributionTypes.find((dis) => dis.distribution_name === value).distribution_params.length
+        ).fill(0)
+      );
+    }
 
   if(name === "distributionValues"){
-      let newArr = this.state.distributionValues
+      let newArr = [... distributionValues]
       newArr[index] = value
 
-      this.setState({
-          distributionValues: newArr
-      })
+      setDistributionValues(newArr)
+      
   } else{
 
-    this.setState({
-      [name]: value
-    });
+    if (name === "unit") setUnit(value);
+    if (name === "distributionType") setDistributionType(value);
+  }
 
   }
 
-  
-  }
+  const changeValueAmount = (amount) => {
+      let newTypes = [... distributionTypes]
 
-  changeValueAmount(amount){
       if(amount === 1){
-          let newTypes = this.state.distributionTypes
-          newTypes.find(dis => dis.distribution_name === "arbitraryFiniteProbabilityDistribution").distribution_params.push("entry" + (this.state.distributionTypes.find(dis => dis.distribution_name === "arbitraryFiniteProbabilityDistribution").distribution_params.length +1))
-         
-
-          this.setState({distributionTypes: newTypes})
-          this.state.distributionValues.push(0)
+          newTypes.find(dis => dis.distribution_name === "arbitraryFiniteProbabilityDistribution").distribution_params.push("entry" + (distributionTypes.find(dis => dis.distribution_name === "arbitraryFiniteProbabilityDistribution").distribution_params.length +1))  
+          setDistributionTypes(newTypes)
+          distributionValues.push(0)
       } else{
-
-          let newTypes = this.state.distributionTypes
           newTypes.find(dis => dis.distribution_name === "arbitraryFiniteProbabilityDistribution").distribution_params.pop()
-         
-
-          this.setState({distributionTypes: newTypes})
-          this.state.distributionValues.pop()
+          setDistributionTypes(newTypes)
+          distributionValues.pop()
       }
     
   }
 
-  handleKeyChange(resource, index) {
+  const handleKeyChange = (resource, index) => {
 
       const target = resource.target;
       const value = target.value;
      
 
-      let params = this.state.distributionTypes
-      params.find(dis => dis.distribution_name === this.state.distributionType).distribution_params[index] = value
+      let params = [... distributionTypes]
+      params.find(dis => dis.distribution_name === distributionType).distribution_params[index] = value
 
-      this.setState({distributionTypes: params})
-
+  
+      setDistributionTypes(params)
 
       console.log(params)
 
       
     }
 
-  onSubmit(event){
+  const onSubmit = (event) =>{
       event.preventDefault();
       
-      const data = [...this.props.getData("allScenario")];
+      const data = [...props.getData("allScenario")];
                 
 
       let interArrivalTime = {
-          distributionType: this.state.distributionType,
-          values: this.state.distributionTypes.find(dis => dis.distribution_name === this.state.distributionType).distribution_params.map((key, index) => {return {id: key, value: this.state.distributionValues[index]}})
+          distributionType: distributionType,
+          values: distributionTypes.find(dis => dis.distribution_name === distributionType).distribution_params.map((key, index) => {return {id: key, value: distributionValues[index]}})
       }
     
-      this.props.getData("currentModel").modelParameter.events.find(
-        value => value.id === this.props.selectedObject.id
+      props.getData("currentModel").modelParameter.events.find(
+        value => value.id === props.selectedObject.id
       ).interArrivalTime = interArrivalTime
 
-      this.props.getData("currentModel").modelParameter.events.find(
-        value => value.id === this.props.selectedObject.id
-      ).unit = this.state.unit
+      props.getData("currentModel").modelParameter.events.find(
+        value => value.id === props.selectedObject.id
+      ).unit = unit
      
 
   
-      this.props.setData(data)
-      console.log(this.props.getData("allData"))      
+      props.setData(data)
+      console.log(props.getData("allData"))      
     }
 
-  render() {
+  
     return (
         <Box w="100%">
-        <form onSubmit={this.onSubmit}>
+        <form onSubmit={onSubmit}>
           <Stack gap="2">        
             <FormControl>
               <FormLabel>Selected Event:</FormLabel>
-              <Input title="Test date" value={this.props.selectedObject.name ? this.props.selectedObject.name : ""} type="inputRead" />
+              <Input title="Test date" value={props.selectedObject.name ? props.selectedObject.name : ""} type="inputRead" />
             </FormControl>
 
           <Text fontWeight="bold" fontSize="md">Interarrival Time:</Text>
           <Flex justifyContent="space-between">
             <FormControl w="47%">
               <FormLabel>Distribution:</FormLabel>
-              <Select value={this.state.distributionType}  bg="white" name="distributionType" onChange={(event) => this.handleInputChange(event)} >
-                {this.state.distributionTypes.map((distribution, index) => {
+              <Select value={distributionType}  bg="white" name="distributionType" onChange={(event) => handleInputChange(event)} >
+                {distributionTypes.map((distribution, index) => {
                     return <option value={distribution.distribution_name}>{distribution.distribution_name}</option>
                 })}
             </Select>
@@ -177,7 +152,7 @@ class Event extends React.Component {
 
           <FormControl w="47%"> 
                 <FormLabel>Time Unit:</FormLabel>
-            <Select name="unit" value={this.state.unit} onChange={(event) => this.handleInputChange(event)} bg="white">
+            <Select name="unit" value={unit} onChange={(event) => handleInputChange(event)} bg="white">
                   <option value='secs'>secs</option>
                   <option value='mins'>mins</option>
               </Select>
@@ -185,14 +160,14 @@ class Event extends React.Component {
             </FormControl>
           </Flex>
 
-            {this.state.distributionType === "arbitraryFiniteProbabilityDistribution" ? 
+            {distributionType === "arbitraryFiniteProbabilityDistribution" ? 
              <ButtonGroup size='md' isAttached variant="outline" >
-                <IconButton icon={<MinusIcon />} onClick={() => this.changeValueAmount(-1)} />
-                <IconButton icon={<AddIcon />} onClick={() => this.changeValueAmount(1)} />
+                <IconButton icon={<MinusIcon />} onClick={() => changeValueAmount(-1)} />
+                <IconButton icon={<AddIcon />} onClick={() => changeValueAmount(1)} />
              </ButtonGroup>
           : ""}
 
-          {this.state.distributionTypes.find((dis) => dis.distribution_name === this.state.distributionType) && this.state.distributionTypes.find(dis => dis.distribution_name === this.state.distributionType).distribution_params.map((key, index) => {
+          {distributionTypes.find((dis) => dis.distribution_name === distributionType) && distributionTypes.find(dis => dis.distribution_name === distributionType).distribution_params.map((key, index) => {
 
         
             return <>
@@ -200,12 +175,12 @@ class Event extends React.Component {
             <Flex justifyContent="space-between">
                 <FormControl w="47%">
                     <FormLabel>key:</FormLabel>
-                    <Input value={key} bg="white"  name="distributionKey" onChange={(event) => this.handleKeyChange(event, index)} />
+                    <Input value={key} bg="white"  name="distributionKey" onChange={(event) => handleKeyChange(event, index)} />
                 </FormControl>
 
                 <FormControl w="47%">
                     <FormLabel>value:</FormLabel>
-                    <Input value={this.state.distributionValues[index]} bg="white" name="distributionValues" onChange={(event) => this.handleInputChange(event, index)} />
+                    <Input value={distributionValues[index]} bg="white" name="distributionValues" onChange={(event) => handleInputChange(event, index)} />
                 </FormControl>
             </Flex></>
 
@@ -226,6 +201,6 @@ class Event extends React.Component {
     );
   }
 
-}
+
 
 export default Event;
